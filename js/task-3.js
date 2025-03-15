762fbd74b6952cb1071d7ddab09fc48d65955e00
Vadim Onoyko <onoykovadim@ukr.net>
2025-03-15 21:01:54 +0200

    todohomework

--- js/task-3.js
+++
@@ -1,8 +0,0 @@
-const hayInput = document.getElementById('name-input');
-const inputName = document.getElementById('name-output');
-const onHayInput = event => {
-  const inputEl = event.currentTarget;
-  console.log(inputEl.value);
-  inputName.textContent = inputEl.value.trim() || 'Anonymous';
-};
-hayInput.addEventListener('input', onHayInput);
